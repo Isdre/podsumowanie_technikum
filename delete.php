67d873760bf73251f316be16b8e40f91208aa939
Gilbert <guszczagilbert@gmail.com>
2022-05-21 20:04:11 +0200

    usuwanie artykułów, poprawa wczytywanie artykułów

---
+++ delete.php
@@ -0,0 +1,11 @@
+<?php
+include('db_connect.php');
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $statement = $mysqli->prepare("DELETE FROM articles WHERE id=? LIMIT 1");
+    $statement->bind_param("i",$id);
+    $statement->execute();
+    $statement->close();
+    header("Location: index.php");
+}
--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
-$email = '';
-$password = '';
-$terms = '';
-$errorEmail = '';
-$errorPassword = '';
-$errorTerms = '';
-$emailSent = '';
-
+$user_id = 0;
+if(isset($_POST['id'])){
+    $user_id = $_POST['id'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="pl">
@@ -18,29 +14,61 @@ $emailSent = '';
     <title>Forum</title>
 </head>
 <body>
-    <header><h1>Forum - </h1></header>
+    <header><h1>Forum</h1></header>
     <div class="container">
+    <div class="account">
+            <?php
+            if($user_id != 0)
+            {
+                echo "<h3>Zalogowany</h3>";
+                $result = $mysqli->query("SELECT * FROM users WHERE id = $user_id ORDER BY id LIMIT 1");
+                foreach($result as $user){
+                echo "<p>Witaj " . $user['username'] . "</p><br>";
+                }
+            }else
+            {
+            echo "<h3>Zaloguj się</h3>";
+                echo "<form method=\"post\" action=\"index.php\">";
+                echo "<input type=\"checkbox\" id=\"LogSing\" name=\"LogSing\" checked hidden>";
+                echo "<label for=\"Login\">Nazwa użytkownika:</label><br>";
+                echo "<input type=\"text\" id=\"login\" name=\"login\"><br>";
+                echo "<label for=\"password\">Hasło:</label><br>";
+                echo "<input type=\"text\" id=\"password\" name=\"password\">";
+                echo "</form>";
+            echo "<h3>Zarejestuj się</h3>";
+                echo "<form method=\"post\" action=\"index.php\">";
+                echo "<input type=\"checkbox\" id=\"LogSing\" name=\"LogSing\" hidden>";                
+                echo "<label for=\"Login\">Nazwa użytkownika:</label><br>";
+                echo "<input type=\"text\" id=\"login\" name=\"login\"><br>";
+                echo "<label for=\"password\">Hasło:</label><br>";
+                echo "<input type=\"text\" id=\"password\" name=\"password\">";
+                echo "</form>";
+            }
+            ?>
+
+        </div>
         <div class="articles">
             <h2>Artykuły</h2>
             <?php
                 include('db_connect.php');
-				$result = $mysqli->query("SELECT * FROM articles JOIN users ON users.id = articles.user_id ORDER BY articles.id DESC");
+				$result = $mysqli->query("SELECT articles.id,title,article,username,user_id FROM articles JOIN users ON users.id = articles.user_id ORDER BY articles.id DESC");
 				foreach($result as $article){
 					echo '<article class="single-article">';
-					echo '<h3>' . $article['title'] . '- napisany przez ' . $article['username'] . '</h3>';
+					echo '<h3>' . $article['title'] . '</h3>';
 					echo '<div class="article-content">';
 					echo '<p>' . $article['article'] . '</p>';
 					echo '</div>';
-					echo '<a href="delete.php?id=' . $article['id'] . '">';
-					echo '<div class="ui label"><i class="remove icon"></i>Usuń/div>';
+                    echo '<h4> Napisany przez ' . $article['username'] . '</h4>';
+                    if($user_id == $article['user_id'] ){
+                    echo '<a class = "delete_button" href="delete.php?id=' . $article['id'] . '">';
+                    echo '<div class="ui label"><i>Usuń</i></div>';
 					echo '</a>';
+                    }
 					echo '</article>';
 				}
 			?>
         </div>
-        <div class="account">
-            
-        </div>
+        
     </div>
     <footer></footer>
 </body>
